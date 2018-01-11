cc00bc378bd955af4745bfbe1e84d5e1ebae8b99
PC201512180928\Administrator <luoluoluoluo@126.com>
2018-01-11 18:01:54 +0800

    update

---
+++ packages/components/layout/header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <header v-show="show">
+            <solt></solt>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            show: Boolean
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
