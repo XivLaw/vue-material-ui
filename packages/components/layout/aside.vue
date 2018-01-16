e59385afd5e34ff62dfc3dce39a40750c9241b15
XivLaw <luoluoluoluo@126.com>
2018-01-17 01:12:15 +0800

    completed header,start aside

--- packages/components/layout/aside.vue
+++ packages/components/layout/aside.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
-
+        <aside></aside>
     </div>
 </template>
 
 <script>
     export default {
         name: 'MtAside',
+        props: {
+            head: {
+                required: true,
+                type: 'String'
+            }
+        }
     }
 </script>
 
 <style scoped lang="less">
-
+    aside{
+        position: fixed;
+        background: #fff;
+        height: calc(100% - 70px);
+        top: 70px;
+        transition: all;
+        -webkit-transition-duration: .3s;
+        transition-duration: .3s;
+        z-index: 10;
+        overflow-y: auto;
+        box-shadow: 0 1px 1px rgba(0,0,0,.15);
+    }
 </style>
