40d78bb63bcf94059f10a1f75ec911c076137a03
PC201512180928\Administrator <luoluoluoluo@126.com>
2018-01-25 17:29:19 +0800

    del less

--- packages/components/layout/header.vue
+++ packages/components/layout/header.vue
@@ -45,7 +45,7 @@ export default {
 };
 </script>
 
-<style scoped lang="less">
+<style scoped>
 #header {
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3);
   user-select: none;
@@ -57,66 +57,63 @@ export default {
   top: 0;
   padding: 17px 20px;
   background-color: #2196f3;
-  div {
-    float: left;
-  }
-  .mt-burger {
-    position: relative;
-    width: 18px;
-    height: 12px;
-    margin: 12px 20px 12px 15px;
-    -webkit-transition: all;
-    -o-transition: all;
-    transition: all;
-    -webkit-transition-duration: 0.3s;
-    transition-duration: 0.3s;
-    &.mt-burger-click {
-      -webkit-transform: rotate(180deg);
-      -ms-transform: rotate(180deg);
-      -o-transform: rotate(180deg);
-      transform: rotate(180deg);
-      .mt-line-top {
-        width: 12px;
-        -webkit-transform: translateX(8px) translateY(1px) rotate(45deg);
-        transform: translateX(8px) translateY(1px) rotate(45deg);
-      }
-      .mt-line-center {
-        top: 5px;
-      }
-      .mt-line-bottom {
-        width: 12px;
-        -webkit-transform: translateX(8px) translateY(-1px) rotate(-45deg);
-        transform: translateX(8px) translateY(-1px) rotate(-45deg);
-      }
-    }
-    span {
-      position: absolute;
-      display: block;
-      width: 18px;
-      height: 2px;
-      transition: all;
-      -webkit-transition-duration: 0.3s;
-      transition-duration: 0.3s;
-      background-color: #fff;
-      &.mt-line-top {
-        top: 0px;
-      }
-      &.mt-line-center {
-        top: 5px;
-      }
-      &.mt-line-bottom {
-        top: 10px;
-      }
-    }
-  }
-  .mt-logo {
-    padding: 7px 10px;
-    text-transform: uppercase;
-    font-size: 16px;
-    a {
-      color: #ffffff;
-    }
-  }
+}
+#header>div {
+  float: left;
+}
+.mt-burger {
+  position: relative;
+  width: 18px;
+  height: 12px;
+  margin: 12px 20px 12px 15px;
+  -webkit-transition: all;
+  -o-transition: all;
+  transition: all;
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+}
+.mt-burger span {
+  position: absolute;
+  display: block;
+  width: 18px;
+  height: 2px;
+  transition: all;
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  background-color: #FFFFFF;
+}
+.mt-line-top {
+  top: 0px;
+}
+.mt-line-center {
+  top: 5px;
+}
+.mt-line-bottom {
+  top: 10px;
+}
+.mt-burger-click {
+  -webkit-transform: rotate(180deg);
+  -ms-transform: rotate(180deg);
+  -o-transform: rotate(180deg);
+  transform: rotate(180deg);
+}
+.mt-burger-click .mt-line-top {
+  width: 12px;
+  -webkit-transform: translateX(8px) translateY(1px) rotate(45deg);
+  transform: translateX(8px) translateY(1px) rotate(45deg);
+}
+.mt-burger-click .mt-line-bottom {
+  width: 12px;
+  -webkit-transform: translateX(8px) translateY(-1px) rotate(-45deg);
+  transform: translateX(8px) translateY(-1px) rotate(-45deg);
+}
+.mt-logo {
+  padding: 7px 10px;
+  text-transform: uppercase;
+  font-size: 16px;
+}
+.mt-logo a {
+  color: #FFFFFF;
 }
 @media all {
   .mt-burger {
--- packages/components/layout/sidebar.vue
+++ packages/components/layout/sidebar.vue
@@ -91,12 +91,12 @@ export default {
 };
 </script>
 
-<style scoped lang="less">
+<style scoped>
 #sidebar {
   position: fixed;
   background: #fff;
   width: 268px;
-  height: calc(~"100% - 70px");
+  height: calc(100% - 70px);
   top: 70px;
   left: 0;
   transition: all;
@@ -105,148 +105,129 @@ export default {
   z-index: 10;
   overflow-y: auto;
   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
-  &.sidebar-show {
-    left: 0px;
-  }
-  .mt-profile {
-    height: 129px;
-    margin-bottom: 5px;
-    width: 100%;
-    background-size: cover;
-    background-position: center center;
-    span {
-      display: block;
-      &.mt-profile-img {
-        padding: 12px;
-        img {
-          width: 47px;
-          height: 47px;
-          border-radius: 50%;
-          border: 3px solid rgba(0, 0, 0, 0.14);
-          box-sizing: content-box;
-        }
-      }
-      &.mt-profile-info {
-        position: relative;
-        padding: 7px 14px;
-        margin-top: 20px;
-        color: #ffffff;
-        background: rgba(0, 0, 0, 0.37);
-      }
-    }
-  }
-  .mt-sidebar-menu {
-    list-style: none;
-    padding-left: 0;
-    margin: 20px 0 0;
-    li {
-      a {
-        position: relative;
-        padding: 14px 20px 14px 65px;
-        display: block;
-        font-weight: 500;
-        color: #4c4c4c;
-        cursor: pointer;
-        -webkit-transition: color;
-        -o-transition: color;
-        transition: color;
-        -webkit-transition-duration: 0.3s;
-        transition-duration: 0.3s;
-        span {
-          position: absolute;
-          left: 25px;
-          font-size: 20px;
-          top: 0;
-          width: 25px;
-          text-align: center;
-          padding: 13px 0;
-        }
-      }
-      &.mt-sub-menu {
-        & > a {
-          &::after,
-          &::before {
-            position: absolute;
-            top: 50%;
-            margin-top: -11px;
-            font-family: Material-Design-Iconic-Font;
-            font-size: 17px;
-            right: 15px;
-            -webkit-transition: all;
-            -o-transition: all;
-            transition: all;
-            -webkit-transition-duration: 250ms;
-            transition-duration: 250ms;
-          }
-          &:after {
-            content: "\f273";
-            -webkit-transform: scale(0);
-            -ms-transform: scale(0);
-            -o-transform: scale(0);
-            transform: scale(0);
-          }
-          &:before {
-            content: "\f278";
-            -webkit-transform: scale(1);
-            -ms-transform: scale(1);
-            -o-transform: scale(1);
-            transform: scale(1);
-          }
-        }
-        ul {
-          overflow: hidden;
-          list-style: none;
-          padding: 0;
-          -webkit-transform: translate3d(0, 0, 0);
-          -moz-transform: translate3d(0, 0, 0);
-          -ms-transform: translate3d(0, 0, 0);
-          -o-transform: translate3d(0, 0, 0);
-          transform: translate3d(0, 0, 0);
-          -webkit-transition: all;
-          -o-transition: all;
-          transition: all;
-          -webkit-transition-duration: 250ms;
-          transition-duration: 250ms;
-          a {
-            padding: 14px 20px 8px 65px;
-            color: #989898;
-          }
-          .active a,
-          a:hover {
-            color: #262626;
-          }
-        }
-      }
-      &.open {
-        & > a {
-          &:after {
-            content: "\f273";
-            -webkit-transform: scale(1);
-            -ms-transform: scale(1);
-            -o-transform: scale(1);
-            transform: scale(1);
-          }
-          &:before {
-            content: "\f278";
-            -webkit-transform: scale(0);
-            -ms-transform: scale(0);
-            -o-transform: scale(0);
-            transform: scale(0);
-          }
-        }
-      }
-    }
-    & > li {
-      &.active > a,
-      & > a:hover {
-        background-color: #f4f4f4;
-      }
-    }
-    .active a,
-    a:hover {
-      color: #262626;
-    }
-  }
+}
+#sidebar.sidebar-show {
+  left: 0px;
+}
+.mt-profile {
+  height: 129px;
+  margin-bottom: 5px;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+}
+.mt-profile span {
+  display: block;
+}
+.mt-profile-img {
+  padding: 12px;
+}
+.mt-profile-img img {
+  width: 47px;
+  height: 47px;
+  border-radius: 50%;
+  border: 3px solid rgba(0, 0, 0, 0.14);
+  box-sizing: content-box;
+}
+.mt-profile-info {
+  position: relative;
+  padding: 7px 14px;
+  margin-top: 20px;
+  color: #ffffff;
+  background: rgba(0, 0, 0, 0.37);
+}
+.mt-sidebar-menu {
+  list-style: none;
+  padding-left: 0;
+  margin: 20px 0 0;
+}
+.mt-sidebar-menu li a {
+  position: relative;
+  padding: 14px 20px 14px 65px;
+  display: block;
+  font-weight: 500;
+  color: #4c4c4c;
+  cursor: pointer;
+  -webkit-transition: color;
+  -o-transition: color;
+  transition: color;
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+}
+.mt-sidebar-menu li a span {
+  position: absolute;
+  left: 25px;
+  font-size: 20px;
+  top: 0;
+  width: 25px;
+  text-align: center;
+  padding: 13px 0;
+}
+.mt-sub-menu a:after,.mt-sub-menu a:before {
+  position: absolute;
+  top: 50%;
+  margin-top: -11px;
+  font-family: Material-Design-Iconic-Font;
+  font-size: 17px;
+  right: 15px;
+  -webkit-transition: all;
+  -o-transition: all;
+  transition: all;
+  -webkit-transition-duration: 250ms;
+  transition-duration: 250ms;
+}
+.mt-sub-menu>a:after{
+  content: "\f273";
+  -webkit-transform: scale(0);
+  -ms-transform: scale(0);
+  -o-transform: scale(0);
+  transform: scale(0);
+}
+.mt-sub-menu>a:before{
+  content: "\f278";
+  -webkit-transform: scale(1);
+  -ms-transform: scale(1);
+  -o-transform: scale(1);
+  transform: scale(1);
+}
+.mt-sub-menu.open>a:after {
+  content: "\f273";
+  -webkit-transform: scale(1);
+  -ms-transform: scale(1);
+  -o-transform: scale(1);
+  transform: scale(1);
+}
+.mt-sub-menu.open>a:before {
+  content: "\f278";
+  -webkit-transform: scale(0);
+  -ms-transform: scale(0);
+  -o-transform: scale(0);
+  transform: scale(0);
+}
+.mt-sidebar-menu>li.active>a,.mt-sidebar-menu>li>a:hover{
+  background-color: #f4f4f4;
+}
+.mt-sidebar-menu .active>a,.mt-sidebar-menu li>a:hover {
+  color: #262626;
+}
+.mt-sub-menu ul {
+  overflow: hidden;
+  list-style: none;
+  padding: 0;
+  -webkit-transform: translate3d(0, 0, 0);
+  -moz-transform: translate3d(0, 0, 0);
+  -ms-transform: translate3d(0, 0, 0);
+  -o-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+  -webkit-transition: all;
+  -o-transition: all;
+  transition: all;
+  -webkit-transition-duration: 250ms;
+  transition-duration: 250ms;
+}
+.mt-sub-menu ul a {
+  padding: 14px 20px 8px 65px;
+  color: #989898;
 }
 @media (max-width: 1279px) {
   #sidebar {
