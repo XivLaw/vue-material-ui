841670da32c41f7824d3d5aaa0672b54533fce68
PC201512180928\Administrator <luoluoluoluo@126.com>
2018-01-26 17:02:56 +0800

    change

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="input-group">
-    <span class="input-group-addon">
+    <span class="input-group-addon" v-if="beforeIcon">
       <i class="zmdi zmdi-arrow-missed"></i>
     </span>
     <div class="fg-line">
         <input type="text" class="form-control" v-bind="$props">
     </div>
-    <span class="input-group-addon last">
+    <span class="input-group-addon last" v-if="afterIcon">
       <i class="zmdi zmdi-globe"></i>
     </span>
   </div>
@@ -16,7 +16,24 @@
   export default {
     name: 'MtInput',
     props: {
-
+      value: [String, Number],
+      placeholder: String,
+      size: String,
+      name: String,
+      form: String,
+      id: String,
+      maxlength: Number,
+      minlength: Number,
+      readonly: Boolean,
+      autofocus: Boolean,
+      disabled: Boolean,
+      autoComplete: {
+        type: String,
+        default: 'off'
+      },
+      step: {},
+      beforeIcon: String,
+      afterIcon: String,
     }
   }
 </script>
