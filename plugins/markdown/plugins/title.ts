d9c40e8db20d66d68121a688eaff469b387c8748
XivLaw <luoluoluoluoluoluoluoluoluoluo@gmail.com>
2022-04-30 20:16:28 +0800

    fix: remove console.log

--- plugins/markdown/plugins/title.ts
+++ plugins/markdown/plugins/title.ts
@@ -5,7 +5,6 @@ export default (md: MarkdownIt, tags = ['h1', 'h2']) => {
   md.renderer.rules.heading_open = (tokens, i, options, _env, self) => {
     const token = tokens[i]
     if (tags.includes(token.tag)) {
-      console.log(tokens, i, options, _env, self);
       return self.renderToken(tokens, i, options);
     }
   };
